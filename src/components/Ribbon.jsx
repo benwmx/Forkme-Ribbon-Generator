a3a5f3ff4b34ae86e3557f5ac58bdbab4e2a5494
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2023-06-04 00:54:58 +0100

    Add dynamic styles to ribbon.jsx

--- src/components/Ribbon.jsx
+++ src/components/Ribbon.jsx
@@ -1,10 +1,87 @@
 import styles from "@/styles/Ribbon.module.css";
-
+import { useRibbonStyleStore } from "@/store";
+import { useState } from "react";
+import { useMediaQuery } from "@react-hook/media-query";
 const Ribbon = () => {
+  const [isHover, setIsHover] = useState(false);
+  const githubUrl = useRibbonStyleStore((state) => state.githubUrl);
+  const ribbonColor = useRibbonStyleStore((state) => state.ribbonColor);
+  const ribbonHoverColor = useRibbonStyleStore(
+    (state) => state.ribbonHoverColor
+  );
+  const textColor = useRibbonStyleStore((state) => state.textColor);
+  const textHoverColor = useRibbonStyleStore((state) => state.textHoverColor);
+  const linesColor = useRibbonStyleStore((state) => state.linesColor);
+  const breakPoint = useRibbonStyleStore((state) => state.breakPoint);
+  const alignment = useRibbonStyleStore((state) => state.alignment);
+  const verticalPosition = useRibbonStyleStore(
+    (state) => state.verticalPosition
+  );
+  const shadowColor = useRibbonStyleStore((state) => state.shadowColor);
+  const shadowHorizontalLength = useRibbonStyleStore(
+    (state) => state.shadowHorizontalLength
+  );
+  const shadowVerticalLength = useRibbonStyleStore(
+    (state) => state.shadowVerticalLength
+  );
+  const blurRadius = useRibbonStyleStore((state) => state.blurRadius);
+  const spreadRadius = useRibbonStyleStore((state) => state.spreadRadius);
+  const isWideScreen = useMediaQuery(`screen and (min-width: ${breakPoint}px)`);
+  const shadowStyle = `${shadowHorizontalLength}px ${shadowVerticalLength}px ${blurRadius}px ${spreadRadius}px rgba(${shadowColor.r}, ${shadowColor.g}, ${shadowColor.b}, ${shadowColor.a})`;
+  const rightAlignmentStyle = {
+    right: "-60px",
+    transform: "rotate(45deg)",
+    WebkitTransform: "rotate(45deg)",
+  };
+  const leftAlignmentStyle = {
+    left: "-60px",
+    transform: "rotate(-45deg)",
+    WebkitTransform: "rotate(-45deg)",
+  };
+  const alignmentStyle =
+    alignment === "right" ? rightAlignmentStyle : leftAlignmentStyle;
+
   return (
-    <span className={styles.ribbon}>
-      <a href="https://www.youtube.com/">Youtube</a>
-    </span>
+    <div
+      style={{
+        position: verticalPosition,
+        right: alignment === "right" ? "0" : "unset",
+        left: alignment === "left" ? "0" : "unset",
+      }}
+      className={isWideScreen ? styles.ribbonInWideScreen : null}
+    >
+      <a
+        href={githubUrl}
+        className={styles.link}
+        style={
+          isHover
+            ? {
+                color: textHoverColor,
+                backgroundColor: ribbonHoverColor,
+                boxShadow: shadowStyle,
+                ...alignmentStyle,
+              }
+            : {
+                color: textColor,
+                backgroundColor: ribbonColor,
+                boxShadow: shadowStyle,
+                ...alignmentStyle,
+              }
+        }
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <span
+          style={{ backgroundColor: linesColor }}
+          className={styles.before}
+        ></span>
+        Fork me on Github
+        <span
+          style={{ backgroundColor: linesColor }}
+          className={styles.after}
+        ></span>
+      </a>
+    </div>
   );
 };
 
